65d976f6ff81131cfb90dceb494cd6124b3302b8
MinahilSadiq1 <113786680+MinahilSadiq1@users.noreply.github.com>
2022-10-17 00:46:36 +0500

    new

--- corona_data.py
+++ corona_data.py
@@ -6,17 +6,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(executable_path='C:/Users/minahil/Desktop/chromedriver.exe')
 driver.get('https://docs.google.com/spreadsheets/d/1nHZopMQpvDO1ETpMVP6ZJ22wk3fHRgB00sekIvqXB_c/pubhtml?gid=1648546042#')
+
 results = []
 
+
 content = driver.page_source
-soup = BeautifulSoup(content, "html5lib")
+soup = BeautifulSoup(content, "html.parser")
 driver.quit()
 
-for a in soup.find_all(attrs='s5'):
-    name = a.find('tbody')
+for a in soup.find_all(attrs='row-headers-background'):
+    name = a.find('tr')
     if name not in results:
         results.append(name.text)
-
 
 df = pd.DataFrame({'Latest data':results})
 df.to_csv('data.csv', index=False, encoding='utf-8')
